5bbe79ff4ec572199722b1c99ab10b8e4bda3446
Akshay Viswanathan <akshayviswa9876@gmail.com>
2024-09-17 21:25:10 +0530

    updated experience

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,20 +5,12 @@ import "./Projects.css";
 
 function Experience() {
   const experienceText = `
-    As a dedicated Software Engineer at Bank Of America, I have been actively involved in designing,
-    developing, and deploying a wide range of software solutions that meet both client and internal business requirements.
-    I specialize in front-end development, with expertise in technologies such as JavaScript, React.
-    My experience extends to creating complex REST APIs, integrating various databases, and implementing efficient
-    backend systems that ensure scalability and performance.
-
-    Throughout my professional journey, I have worked closely with cross-functional teams, applying Agile methodologies
-    to deliver high-quality products within deadlines. My strong problem-solving skills and a passion for learning new
-    technologies have enabled me to contribute significantly to a variety of challenging projects, from web development
-    and cloud-based solutions to optimizing machine learning models.
-
-    In addition to my technical capabilities, I am passionate about mentoring junior developers, enhancing collaboration
-    within teams, and continuously improving software development processes. I thrive in dynamic environments, where my
-    curiosity and drive to innovate enable me to deliver robust, scalable, and user-centric solutions.
+I joined Bank of America in Chennai on July 17th, 2023, and it’s been an incredible year since then.
+ The company offers flexible working hours and encourages participation in various activities beyond regular project work. 
+ I’ve been involved in employee networks like Women in Technology and Operations, ICUBE, and ESG, among others. 
+ Bank of America also supports Hackathons and Ideathons, fostering innovative solutions for a better future. 
+ I’ve had the chance to learn new languages like C# and work with platforms such as SQL Server, Autosys, Atlassian Bitbucket, 
+ Jenkins, Ansible, Celestial, and RISE.
   `;
 
   return (
